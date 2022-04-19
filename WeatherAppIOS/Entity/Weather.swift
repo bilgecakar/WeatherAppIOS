434456bff9bf76b863c9a09024fca67922814437
Bilge Çakar <36795459+bilgecakar@users.noreply.github.com>
2022-04-19 22:24:21 +0300

    Add Entity

---
+++ WeatherAppIOS/Entity/Weather.swift
@@ -0,0 +1,18 @@
+//
+//  Weather.swift
+//  WeatherAppIOS
+//
+//  Created by Bilge Çakar on 19.04.2022.
+//
+
+import Foundation
+
+class Weather
+{
+    var temp : Double?
+    var city_name : String?
+    var wind_spd : Double?
+    var wind_cdir_full : String?
+    var wind_cdir : String?
+               
+}
